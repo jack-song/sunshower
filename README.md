2d0e433f700610ba3651bdd3befd052fc0ab8d47
Jack Song <jackthesong@gmail.com>
2017-05-22 09:29:36 -0400

    add analytics add versioning deploy

--- README.md
+++ README.md
@@ -8,9 +8,6 @@
 Made with phaser, https://github.com/lean/phaser-es6-webpack.
 
 ## Todo
-- google analytics
-- ship
-
 - tweened animations
 - less memory usage
 - keep piece colors
--- src/states/Menu.js
+++ src/states/Menu.js
@@ -6,7 +6,7 @@ import config from '../config'
 export default class extends Phaser.State {
   create () {
     this.stage.backgroundColor = config.BACKGROUND_COLOR;
-    utils.addMenuItem(30, "SunShower", -100, this);
+    utils.addMenuItem(30, "SunShower v0.1", -100, this);
 
     utils.addMenuItem(20, "Start", 0, this, () => {
       this.state.start('Game');
